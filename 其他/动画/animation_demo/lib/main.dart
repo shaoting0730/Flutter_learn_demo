307882888d9dd10fdf6ff1dd086b31d9cfe39068
pheromone <510738319@qq.com>
2019-01-16 20:43:50 +0800

    保存一下 睡觉 😴
    保存一下 睡觉 😴

---
+++ 其他/动画/animation_demo/lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: ScaleAnimationRoute(),
+    );
+  }
+}
+
+
+class ScaleAnimationRoute extends StatefulWidget {
+  @override
+  _ScaleAnimationRouteState createState() => new _ScaleAnimationRouteState();
+}
+
+class _ScaleAnimationRouteState extends State<ScaleAnimationRoute>
+    with SingleTickerProviderStateMixin {
+  Animation<double> animation;
+  AnimationController controller;
+
+  initState() {
+    super.initState();
+    controller = new AnimationController(
+        duration: const Duration(seconds: 3), vsync: this);
+    //图片宽高从0变到300
+    animation = new Tween(begin: 0.0, end: 300.0).animate(controller);
+    //启动动画
+    controller.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //return AnimatedImage(animation: animation,);
+    return Scaffold(
+      appBar: AppBar(title: Text('动画demo1')),
+      body: AnimatedBuilder(
+        animation: animation,
+        child: Image.network("http://ww1.sinaimg.cn/large/0065oQSqly1fsq9iq8ttrj30k80q9wi4.jpg"),
+        builder: (BuildContext ctx, Widget child) {
+          return new Center(
+            child: Container(
+              height: animation.value,
+              width: animation.value,
+              child: child,
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  dispose() {
+    //路由销毁时需要释放动画资源
+    controller.dispose();
+    super.dispose();
+  }
+}
