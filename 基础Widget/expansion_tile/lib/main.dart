12af9f87d555a0403f1b804388be4a0f73364912
pheromone <510738319@qq.com>
2019-01-30 20:06:16 +0800

    add 注释
    add 注释

--- 基础Widget/expansion_tile/lib/main.dart
+++ 基础Widget/expansion_tile/lib/main.dart
@@ -18,6 +18,16 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+/*
+ * ExpansionTile Widget就是一个可以展开闭合的组件，常用的属性有如下几个。
+
+title:闭合时显示的标题，这个部分经常使用Text Widget。
+leading:标题左侧图标，多是用来修饰，让界面显得美观。
+backgroundColor: 展开时的背景颜色，当然也是有过度动画的，效果非常好。
+children: 子元素，是一个数组，可以放入多个元素。
+trailing ： 右侧的箭头，你可以自行替换但是我觉的很少替换，因为谷歌已经表现的很完美了。
+initiallyExpanded: 初始状态是否展开，为true时，是展开，默认为false，是不展开。 
+ */
 
 class ExpansionTileDemo extends StatelessWidget {
   @override
