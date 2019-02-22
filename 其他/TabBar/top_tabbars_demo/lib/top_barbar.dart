3fe20f1c2e6a15e2b6e5ade8e430d3e8c23f27d1
pheromone <510738319@qq.com>
2019-02-22 11:40:19 +0800

    add 上方tabBar 🌺🐟 🌺🐟 🌺🐟
    add 上方tabBar 🌺🐟 🌺🐟 🌺🐟

---
+++ 其他/TabBar/top_tabbars_demo/lib/top_barbar.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class TopBar extends StatefulWidget {
+  _TopBarState createState() => _TopBarState();
+}
+
+class _TopBarState extends State<TopBar> with SingleTickerProviderStateMixin {
+  TabController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TabController(length: 10, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('topBar'),
+        bottom: TabBar(
+          controller: _controller,
+          isScrollable: true,
+          
+          tabs: <Widget>[
+            Tab(text: '壹'),
+            Tab(text: '贰'),
+            Tab(text: '叄'),
+            Tab(text: '肆'),
+            Tab(text: '伍'),
+            Tab(text: '陆'),
+            Tab(text: '柒'),
+            Tab(text: '捌'),
+            Tab(text: '玖'),
+            Tab(icon:Icon(Icons.business_center)),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: _controller,
+        children: <Widget>[
+          Text('11'),
+          Text('22'),
+          Text('33'),
+          Text('44'),
+          Text('55'),
+          Text('66'),
+          Text('77'),
+          Text('88'),
+          Text('99'),
+          Text('100'),
+        ],
+      ),
+    );
+  }
+}
