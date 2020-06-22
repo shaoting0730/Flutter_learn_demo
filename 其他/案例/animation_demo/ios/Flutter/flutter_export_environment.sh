7a033eb25b7b2ee083bdd4a13c05a54f01212d9b
少停 <510738319@qq.com>
2020-06-22 19:33:37 +0800

    。。
    。。

--- 其他/案例/animation_demo/ios/Flutter/flutter_export_environment.sh
+++ 其他/案例/animation_demo/ios/Flutter/flutter_export_environment.sh
@@ -5,6 +5,7 @@ export "FLUTTER_APPLICATION_PATH=/Users/zhoushaoting/Desktop/Flutter学习/其�
 export "FLUTTER_TARGET=/Users/zhoushaoting/Desktop/Flutter学习/其他/案例/animation_demo/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
 export "FLUTTER_FRAMEWORK_DIR=/Users/zhoushaoting/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
