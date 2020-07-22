e1f54f4dbb59f1d871faf20c93e801392faa322c
少停 <510738319@qq.com>
2020-07-22 22:46:51 +0800

    去除tabbar的默认点击效果
    去除tabbar的默认点击效果

--- 其他/TabBar/tabbar_demo/lib/tabbar.dart
+++ 其他/TabBar/tabbar_demo/lib/tabbar.dart
@@ -3,7 +3,6 @@ import 'package:tabbar_demo/pages/homepage.dart';
 import 'package:tabbar_demo/pages/showpage.dart';
 import 'package:tabbar_demo/pages/minepage.dart';
 
- 
 class Tabbar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -11,14 +10,14 @@ class Tabbar extends StatelessWidget {
         debugShowCheckedModeBanner: false, home: new MainPageWidget());
   }
 }
- 
+
 class MainPageWidget extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return new MainPageState();
   }
 }
- 
+
 class MainPageState extends State<MainPageWidget> {
   int _tabIndex = 0;
   var tabImages;
@@ -27,7 +26,7 @@ class MainPageState extends State<MainPageWidget> {
    * 存放三个页面，跟fragmentList一样
    */
   var _pageList;
- 
+
   /*
    * 根据选择获得对应的normal或是press的icon
    */
@@ -37,6 +36,7 @@ class MainPageState extends State<MainPageWidget> {
     }
     return tabImages[curIndex][0];
   }
+
   /*
    * 获取bottomTab的颜色和文字
    */
@@ -49,22 +49,31 @@ class MainPageState extends State<MainPageWidget> {
           style: new TextStyle(fontSize: 14.0, color: const Color(0xff515151)));
     }
   }
+
   /*
    * 根据image路径获取图片
    */
   Image getTabImage(path) {
     return new Image.asset(path, width: 24.0, height: 24.0);
   }
- 
- 
+
   void initData() {
     /*
      * 初始化选中和未选中的icon
      */
     tabImages = [
-      [getTabImage('images/tab/home.png'), getTabImage('images/tab//home_select.png')],
-      [getTabImage('images/tab/show.png'), getTabImage('images/tab//show_select.png')],
-      [getTabImage('images/tab/mine.png'), getTabImage('images/tab//mine_select.png')]
+      [
+        getTabImage('images/tab/home.png'),
+        getTabImage('images/tab//home_select.png')
+      ],
+      [
+        getTabImage('images/tab/show.png'),
+        getTabImage('images/tab//show_select.png')
+      ],
+      [
+        getTabImage('images/tab/mine.png'),
+        getTabImage('images/tab//mine_select.png')
+      ]
     ];
     /*
      * 三个子界面
@@ -75,32 +84,37 @@ class MainPageState extends State<MainPageWidget> {
       new Minepage(),
     ];
   }
- 
+
   @override
   Widget build(BuildContext context) {
     //初始化数据
     initData();
     return Scaffold(
         body: _pageList[_tabIndex],
-        bottomNavigationBar: new BottomNavigationBar(
-          items: <BottomNavigationBarItem>[
-            new BottomNavigationBarItem(
-                icon: getTabIcon(0), title: getTabTitle(0)),
-            new BottomNavigationBarItem(
-                icon: getTabIcon(1), title: getTabTitle(1)),
-            new BottomNavigationBarItem(
-                icon: getTabIcon(2), title: getTabTitle(2)),
-          ],
-          type: BottomNavigationBarType.fixed,
-          //默认选中首页
-          currentIndex: _tabIndex,
-          iconSize: 24.0,
-          //点击事件
-          onTap: (index) {
-            setState(() {
-              _tabIndex = index;
-            });
-          },
-        ));
+        bottomNavigationBar: Theme(
+            data: ThemeData(
+              splashColor: Colors.transparent,
+              highlightColor: Colors.transparent,
+            ),
+            child: BottomNavigationBar(
+              items: <BottomNavigationBarItem>[
+                new BottomNavigationBarItem(
+                    icon: getTabIcon(0), title: getTabTitle(0)),
+                new BottomNavigationBarItem(
+                    icon: getTabIcon(1), title: getTabTitle(1)),
+                new BottomNavigationBarItem(
+                    icon: getTabIcon(2), title: getTabTitle(2)),
+              ],
+              type: BottomNavigationBarType.fixed,
+              //默认选中首页
+              currentIndex: _tabIndex,
+              iconSize: 24.0,
+              //点击事件
+              onTap: (index) {
+                setState(() {
+                  _tabIndex = index;
+                });
+              },
+            )));
   }
 }
