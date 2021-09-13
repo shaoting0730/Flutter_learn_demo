f49640e5fe46463c6bf18e26d00d910144736d86
pheromone <510738319@qq.com>
2021-09-13 13:51:12 +0800

    。。
    。。

---
+++ 布局Widget/rowwidget_demo/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/zhoushaoting/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/zhoushaoting/Desktop/Flutter学习/布局Widget/rowwidget_demo"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/zhoushaoting/Desktop/Flutter学习/布局Widget/rowwidget_demo/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/zhoushaoting/Desktop/Flutter学习/布局Widget/rowwidget_demo/.dart_tool/package_config.json"
