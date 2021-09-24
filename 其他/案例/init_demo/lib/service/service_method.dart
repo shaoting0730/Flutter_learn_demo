098a616c7017dd993f12417432b5050e210683e8
pheromone <510738319@qq.com>
2021-09-24 11:48:53 +0800

    ..
    ..

--- 其他/案例/init_demo/lib/service/service_method.dart
+++ 其他/案例/init_demo/lib/service/service_method.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:get/route_manager.dart';
 import 'package:init_demo/main.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -27,8 +28,31 @@ class DioUtil {
         throw Exception('后端接口出现异常');
       }
     } catch (e) {
-      // BuildContext context = navigatorKey.currentState!.context;
-      Fluttertoast.showToast(msg: '请求$url失败');
+      BuildContext context = navigatorKey.currentState!.context;
+      // Fluttertoast.showToast(msg: '请求$url失败');
+      showDialog(
+        context: context,
+        builder: (context) {
+          return SimpleDialog(
+            title: const Text('是否选择再次请求'),
+            children: [
+              SimpleDialogOption(
+                child: const Text('再请求一次'),
+                onPressed: () {
+                  get(url);
+                  Navigator.of(context).pop();
+                },
+              ),
+              SimpleDialogOption(
+                child: const Text('算了'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              )
+            ],
+          );
+        },
+      );
       return print(e);
     }
   }
