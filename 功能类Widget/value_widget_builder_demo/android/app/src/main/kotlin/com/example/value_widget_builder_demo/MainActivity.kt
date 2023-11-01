0e8f12c5667b4efa16af1aa92e2f6bd93a364f91
zhoushaoting <510738319@qq.com>
2023-11-01 15:16:58 +0800

    valueWidgetBuilder

---
+++ 功能类Widget/value_widget_builder_demo/android/app/src/main/kotlin/com/example/value_widget_builder_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.value_widget_builder_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ 功能类Widget/value_widget_builder_demo/lib/main.dart
@@ -0,0 +1,107 @@
+/*
+ * @Author: zhoushaoting 510738319@qq.com
+ * @Date: 2023-11-01 13:33:28
+ * @LastEditors: zhoushaoting 510738319@qq.com
+ * @LastEditTime: 2023-11-01 13:51:57
+ * @FilePath: /value_widget_builder_demo/lib/main.dart
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a blue toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  // 定义 ValueNotifier 对象 _counter
+  final ValueNotifier<int> _counter = ValueNotifier<int>(0);
+  int a = 1;
+
+  @override
+  void dispose() {
+    _counter.dispose();
+    super.dispose();
+  }
+
+  void _incrementCounter() {
+    _counter.value += 1;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('+++++++++++++++++++++++++render了+++++++++++++++++++++++++');
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('You have pushed the button this many times:'),
+            ValueListenableBuilder<int>(
+              builder: (BuildContext context, int value, Widget? child) {
+                return Text('$value');
+              },
+              valueListenable: _counter,
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
