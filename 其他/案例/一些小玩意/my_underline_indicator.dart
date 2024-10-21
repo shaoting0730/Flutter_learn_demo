1af07cbbdec4b4d106b8993dd03dec6c2b4e8b4a
ʱªʱªʱª <510738319@qq.com>
2024-10-21 14:28:48 +0800

    Update my_underline_indicator.dart

--- 其他/案例/一些小玩意/my_underline_indicator.dart
+++ 其他/案例/一些小玩意/my_underline_indicator.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'dart:ui' as ui;
 
 class UnderlineDecoration extends Decoration {
   final Color color;
   final double? thickness;
   final double? length;
+  final ui.Image? image;
 
-  UnderlineDecoration({required this.color, this.thickness, this.length});
+  const UnderlineDecoration(
+      {required this.color, this.thickness, this.length, this.image});
 
   @override
   BoxPainter createBoxPainter([VoidCallback? onChanged]) {
-    return _UnderlinePainter(color: color, thickness: thickness, length: length);
+    return _UnderlinePainter(
+        color: color, thickness: thickness, length: length, image: image);
   }
 }
 
@@ -17,22 +22,38 @@ class _UnderlinePainter extends BoxPainter {
   final Paint _paint;
   final double _thickness;
   final double _length;
+  final ui.Image? _image;
 
-  _UnderlinePainter({required Color color, double? thickness, double? length})
+  _UnderlinePainter(
+      {required Color color,
+      double? thickness,
+      double? length,
+      ui.Image? image})
       : _paint = Paint()
-    ..color = color
-    ..style = PaintingStyle.fill,
+          ..color = color
+          ..style = PaintingStyle.fill,
         _thickness = thickness ?? 1.0,
-        _length = length ?? 20.0;
+        _length = length ?? 20.w,
+        _image = image;
 
   @override
   void paint(Canvas canvas, Offset offset, ImageConfiguration configuration) {
     final Rect rect = Offset(
-      offset.dx + (configuration.size!.width - _length) / 2,
-      configuration.size!.height - _thickness,
-    ) &
-    Size(_length, _thickness);
-
-    canvas.drawRect(rect, _paint);
+          offset.dx + (configuration.size!.width - _length) / 2,
+          configuration.size!.height - _thickness,
+        ) &
+        Size(_length, _thickness);
+    RRect rRect =
+        RRect.fromRectAndRadius(rect, Radius.circular(_thickness / 2));
+    if (_image == null) {
+      canvas.drawRRect(rRect, _paint);
+    } else {
+      canvas.drawImage(
+          _image,
+          Offset(offset.dx + (configuration.size!.width - _length) / 2,
+              configuration.size!.height - _thickness - 5),
+          _paint);
+    }
   }
 }
+
